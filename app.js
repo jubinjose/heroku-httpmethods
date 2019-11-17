e5aa17e1552061061beac01ee95edfe41e6098b2
jubin <jubin@jubin.net>
2019-11-17 11:20:27 -0500

    Enabled CORS Add get, post, put, delete endpoints for respective operations

--- app.js
+++ app.js
@@ -2,11 +2,33 @@ let port = process.env.PORT; // On Heroku this will be the PORT app can listen t
 if (port == null || port == "") {
   port = 3000;
 }
+
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Http Get');
+
+//Enable CORS for all resources
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.get('/get', function (req, res) {
+  res.send('HTTP GET');
+});
+
+app.post('/post', function (req, res) {
+  res.send('HTTP POST');
+});
+
+app.put('/put', function (req, res) {
+  res.send('HTTP PUT');
+});
+
+app.delete('/delete', function (req, res) {
+  res.send('HTTP DELETE');
+});
+
 app.listen(port, function () {
   console.log('Listening on port ' + port);
 });
